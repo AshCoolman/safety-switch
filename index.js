55c943afc3259a534b6beeadf4683c3060cf1df5
A Coleman <writetofish@gmail.com>
2015-12-14 13:49:06 +0000

    update: Exist message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function safetySwitchTriggerCheck() {
     if (process.env.SAFETY_SWITCH_IS_RELEASED!=="true") {
-        throw "SAFETY_SWITCH_IS_RELEASED does not exist - exiting!"
+        throw "SAFETY_SWITCH_IS_RELEASED is not set to `true` - exiting!"
         process.exit(1);
     }
 }
